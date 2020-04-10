8deb7d580b676ed7ee36f5d1cea2bd97099eb376
CommanderJames <41754682+CommanderJames@users.noreply.github.com>
2020-04-10 15:09:34 +0530

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,10 @@
+class BaseMessage(object):
+    base_size = 20
+    def __init__(self, sender, data=None):
+        self.sender = sender
+        self.data = data
+    @property
+    def size(self):
+        return self.base_size + len(repr(self.data))
+    def __repr__(self):
+        return '<%s>' % self.__class__.__name__
